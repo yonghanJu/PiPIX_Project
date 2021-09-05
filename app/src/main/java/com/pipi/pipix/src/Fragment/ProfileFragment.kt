e5991b621f53bf5a2fcd0b9cb3dcd0501e44b985
changs97 <pck4949@naver.com>
2021-09-05 15:28:25 +0900

    Update layout

--- app/src/main/java/com/pipi/pipix/src/Fragment/ProfileFragment.kt
+++ app/src/main/java/com/pipi/pipix/src/Fragment/ProfileFragment.kt
@@ -3,6 +3,7 @@ package com.pipi.pipix.src.Fragment
 import android.os.Bundle
 import android.view.View
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.pipi.pipix.R
 import com.pipi.pipix.config.ApplicationClass
 import com.pipi.pipix.config.BaseFragment
@@ -13,6 +14,21 @@ class ProfileFragment : BaseFragment<FragmentProfileBinding>(FragmentProfileBind
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val resultList = arrayListOf("2021.09.05 오전 12시 39분","2021.09.05 오전 12시 39분","2021.09.05 오전 12시 39분",
+            "2021.09.05 오전 12시 39분","2021.09.05 오전 12시 39분","2021.09.05 오전 12시 39분")
+
+        // 리사이클러뷰에 LinearLayoutManager 객체 지정.
+        val recyclerView = binding.profileRecyclerviewResult
+        recyclerView.setLayoutManager(object : LinearLayoutManager(context){
+            override fun canScrollVertically(): Boolean {
+                return true
+            }
+        })
+
+        // 리사이클러뷰에 Adapter 객체 지정.
+        var profileRecyclerviewAdapter = ProfileRecyclerviewAdapter(this, resultList)
+        recyclerView.adapter = profileRecyclerviewAdapter
 
         binding.profileTextviewNickname.text = ApplicationClass.prefs.userNickName
 
