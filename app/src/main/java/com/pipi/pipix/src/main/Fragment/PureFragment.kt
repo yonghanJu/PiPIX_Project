1e4925dfe18fc3332589057ac212169833c014e2
changs97 <pck4949@naver.com>
2021-09-07 15:06:03 +0900

    Create PureFragment.kt

---
+++ app/src/main/java/com/pipi/pipix/src/main/Fragment/PureFragment.kt
@@ -0,0 +1,17 @@
+package com.pipi.pipix.src.main.Fragment
+
+import android.os.Bundle
+import android.view.View
+import com.pipi.pipix.R
+import com.pipi.pipix.config.BaseFragment
+import com.pipi.pipix.databinding.FragmentPureBinding
+
+class PureFragment : BaseFragment<FragmentPureBinding>(FragmentPureBinding::bind, R.layout.fragment_pure) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+
+}
