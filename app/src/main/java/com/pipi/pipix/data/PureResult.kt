f1b9f39430ebece449aa91bcae43687781b2a356
yonghanJu <ju990828@naver.com>
2021-09-09 23:38:51 +0900

    Update PureResult.kt

--- app/src/main/java/com/pipi/pipix/data/PureResult.kt
+++ app/src/main/java/com/pipi/pipix/data/PureResult.kt
@@ -8,7 +8,8 @@ import androidx.room.PrimaryKey
 data class PureResult (
     @PrimaryKey(autoGenerate = true)
     val id: Int,
-    val tpa: Int,
+    val tpaRight: Int,
+    val tpaLeft: Int,
     val date: String,
 
     val R_250: Int?,
