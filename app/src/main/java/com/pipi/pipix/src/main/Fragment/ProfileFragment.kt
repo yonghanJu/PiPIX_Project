c3f25d1db6f4641b876e6f0f3a8aa33aacadb3f4
changs97 <pck4949@naver.com>
2021-09-10 15:25:04 +0900

    Update ProfileFragment.kt

--- app/src/main/java/com/pipi/pipix/src/main/Fragment/ProfileFragment.kt
+++ app/src/main/java/com/pipi/pipix/src/main/Fragment/ProfileFragment.kt
@@ -57,8 +57,8 @@ class ProfileFragment : BaseFragment<FragmentProfileBinding>(FragmentProfileBind
         val sdf =  SimpleDateFormat("yyyy.MM.dd a hh시 mm분")
         val getTime = sdf.format(date)
 
-        val testPureData = PureResult(0,0,getTime,0,0,0,0,0,0,0,0,0,0,
-                0,0,0,0,0,0,0,0,0,0)
+        val testPureData = PureResult(0,0,0,getTime,0,0,0,0,0,0,0,0,0,
+                0,0,0,0,0,0,0,0,0,0,0)
 
 
         mUserViewModel.addPureResult(testPureData)
