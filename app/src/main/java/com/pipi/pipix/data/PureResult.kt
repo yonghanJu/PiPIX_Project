28ecabde182246101877c4687201320d97e3edb1
yonghanJu <ju990828@naver.com>
2021-09-12 17:41:08 +0900

    Update PureResult.kt

--- app/src/main/java/com/pipi/pipix/data/PureResult.kt
+++ app/src/main/java/com/pipi/pipix/data/PureResult.kt
@@ -14,26 +14,21 @@ data class PureResult (
     val tpaLeft: Int,
     val date: String,
 
+    val totalCount: Int,
+    val correctCount: Int,
+
     
     val R_250: Int? = null,
     val R_500: Int? = null,
-    val R_750: Int? = null,
     val R_1000: Int? = null,
-    val R_1500: Int? = null,
     val R_2000: Int? = null,
-    val R_3000: Int? = null,
     val R_4000: Int? = null,
-    val R_6000: Int? = null,
     val R_8000: Int? = null,
 
     val L_250: Int? = null,
     val L_500: Int? = null,
-    val L_750: Int? = null,
     val L_1000: Int? = null,
-    val L_1500: Int? = null,
     val L_2000: Int? = null,
-    val L_3000: Int? = null,
     val L_4000: Int? = null,
-    val L_6000: Int? = null,
     val L_8000: Int? = null
 ) : Serializable
