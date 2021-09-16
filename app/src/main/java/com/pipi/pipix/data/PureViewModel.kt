9993ac1d02ea89b7ffe98db41579011948833518
yonghanJu <ju990828@naver.com>
2021-09-16 15:27:18 +0900

    Update PureViewModel.kt

--- app/src/main/java/com/pipi/pipix/data/PureViewModel.kt
+++ app/src/main/java/com/pipi/pipix/data/PureViewModel.kt
@@ -5,8 +5,6 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class PureViewModel:ViewModel() {
-
-
     val currentCountVisible: MutableLiveData<Int> by lazy{
         MutableLiveData<Int>()
     }
