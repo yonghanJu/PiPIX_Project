ad46bc07cb7cfcda6009789124d52e48b21ee364
changs97 <pck4949@naver.com>
2021-09-19 20:28:30 +0900

    Update ProfileFragment.kt

--- app/src/main/java/com/pipi/pipix/src/main/Fragment/ProfileFragment.kt
+++ app/src/main/java/com/pipi/pipix/src/main/Fragment/ProfileFragment.kt
@@ -3,11 +3,12 @@ package com.pipi.pipix.src.main.fragment
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.LinearLayout
+import androidx.core.view.GravityCompat
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.pipi.pipix.R
+
 import com.pipi.pipix.config.ApplicationClass
 import com.pipi.pipix.config.BaseFragment
 import com.pipi.pipix.data.PRViewModel
@@ -24,6 +25,10 @@ import kotlinx.coroutines.test.withTestContext
 import java.text.SimpleDateFormat
 import java.util.*
 
+import androidx.drawerlayout.widget.DrawerLayout
+import com.pipi.pipix.R
+
+
 class ProfileFragment : BaseFragment<FragmentProfileBinding>(FragmentProfileBinding::bind, R.layout.fragment_profile) {
 
 
@@ -32,11 +37,37 @@ class ProfileFragment : BaseFragment<FragmentProfileBinding>(FragmentProfileBind
         var testType : Int? = null // 1이면 순음 2이면 어음
         var dataList = emptyList<PureResult>()
         lateinit var mUserViewModel : PRViewModel
+        var drawerLayout : DrawerLayout? = null
+        var drawerView : LinearLayout? = null
 
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        // 전체화면인 DrawerLayout 객체 참조
+        drawerLayout = binding.drawerLayout
+
+        // Drawer 화면(뷰) 객체 참조
+        drawerView = binding.profileLayoutDrawer
+
+        binding.profileDrawerTextviewNickname.text = ApplicationClass.prefs.userNickName
+
+
+        // 드로어 화면을 열고 닫을 버튼 객체 참조
+
+        val btnOpenDrawer = binding.profileImageButtonOpendrawer
+        val btnCloseDrawer= binding.profileDrawerButtonClosedrawer
+
+
+        // 드로어 여는 버튼 리스너
+        btnOpenDrawer.setOnClickListener(View.OnClickListener { drawerLayout!!.openDrawer(drawerView!!) })
+
+        // 드로어 닫는 버튼 리스너
+        btnCloseDrawer.setOnClickListener(View.OnClickListener { drawerLayout!!.closeDrawer(drawerView!!) })
+
+
+
 
         testType = null//뒤로가기로 인해 다시 화면에 돌아오면 null 처리
 
@@ -49,6 +80,7 @@ class ProfileFragment : BaseFragment<FragmentProfileBinding>(FragmentProfileBind
 
 
         binding.profileTextviewNickname.text = ApplicationClass.prefs.userNickName
+
 
         binding.profileTextviewMenu4.setOnClickListener {
             findNavController().navigate(R.id.action_ProfileFragment_to_resultFragment)
@@ -70,4 +102,5 @@ class ProfileFragment : BaseFragment<FragmentProfileBinding>(FragmentProfileBind
                 }
             }
         }
+
     }
