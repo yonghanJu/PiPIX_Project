3030d26ceff2af7a7e74422cd2546088db9ab400
yonghanJu <ju990828@naver.com>
2021-09-26 22:42:16 +0900

    Update SpeechViewModel.kt

--- app/src/main/java/com/pipi/pipix/testpackage/SpeechViewModel.kt
+++ app/src/main/java/com/pipi/pipix/testpackage/SpeechViewModel.kt
@@ -15,4 +15,8 @@ class SpeechViewModel: ViewModel() {
     val currentClickable: MutableLiveData<Boolean> by lazy{
         MutableLiveData<Boolean>()
     }
+
+    val currentProgress: MutableLiveData<Int> by lazy{
+        MutableLiveData<Int>()
+    }
 }
